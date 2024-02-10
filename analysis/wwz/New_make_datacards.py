8cfab2b7b56be539a29c26be8fa2ee971ab3610f
Kelci <k.mohrman@ufl.edu>
2024-02-10 18:32:34 -0500

    Add some kappa stuff

--- analysis/wwz/New_make_datacards.py
+++ analysis/wwz/New_make_datacards.py
@@ -1,3 +1,4 @@
+import numpy as np
 import os
 import json
 import pickle
@@ -18,18 +19,18 @@ CAT_LST_CB = ["sr_4l_sf_A", "sr_4l_sf_B", "sr_4l_sf_C", "sr_4l_of_1", "sr_4l_of_
 
 # Systs that are not correlated across years
 SYSTS_SPECIAL = {
-    "btagSFlight_uncorrelated_2016APV" : {"yr_rel":"UL16APV", "yr_notrel": ["UL16", "UL17", "UL18"]},
+    #"btagSFlight_uncorrelated_2016APV" : {"yr_rel":"UL16APV", "yr_notrel": ["UL16", "UL17", "UL18"]},
     "btagSFbc_uncorrelated_2016APV"    : {"yr_rel":"UL16APV", "yr_notrel": ["UL16", "UL17", "UL18"]},
-    "btagSFlight_uncorrelated_2016"    : {"yr_rel":"UL16", "yr_notrel": ["UL16APV", "UL17", "UL18"]},
-    "btagSFbc_uncorrelated_2016"       : {"yr_rel":"UL16", "yr_notrel": ["UL16APV", "UL17", "UL18"]},
-    "btagSFlight_uncorrelated_2017"    : {"yr_rel":"UL17", "yr_notrel": ["UL16APV", "UL16", "UL18"]},
-    "btagSFbc_uncorrelated_2017"       : {"yr_rel":"UL17", "yr_notrel": ["UL16APV", "UL16", "UL18"]},
-    "btagSFlight_uncorrelated_2018"    : {"yr_rel":"UL18", "yr_notrel": ["UL16APV", "UL16", "UL17"]},
-    "btagSFbc_uncorrelated_2018"       : {"yr_rel":"UL18", "yr_notrel": ["UL16APV", "UL16", "UL17"]},
+    #"btagSFlight_uncorrelated_2016"    : {"yr_rel":"UL16", "yr_notrel": ["UL16APV", "UL17", "UL18"]},
+    #"btagSFbc_uncorrelated_2016"       : {"yr_rel":"UL16", "yr_notrel": ["UL16APV", "UL17", "UL18"]},
+    #"btagSFlight_uncorrelated_2017"    : {"yr_rel":"UL17", "yr_notrel": ["UL16APV", "UL16", "UL18"]},
+    #"btagSFbc_uncorrelated_2017"       : {"yr_rel":"UL17", "yr_notrel": ["UL16APV", "UL16", "UL18"]},
+    #"btagSFlight_uncorrelated_2018"    : {"yr_rel":"UL18", "yr_notrel": ["UL16APV", "UL16", "UL17"]},
+    #"btagSFbc_uncorrelated_2018"       : {"yr_rel":"UL18", "yr_notrel": ["UL16APV", "UL16", "UL17"]},
 }
 
 
-# Get the yields
+# Get the yields (nested in the order: year, category, syst, proc)
 def get_yields(histo,sample_dict,blind=True,systematic_name=None):
 
     yld_dict = {}
@@ -41,6 +42,8 @@ def get_yields(histo,sample_dict,blind=True,systematic_name=None):
     for cat_name in histo.axes["category"]:
         yld_dict[cat_name] = {}
         for syst_name in syst_lst:
+            #if syst_name not in ["nominal", "btagSFlight_correlatedUp", "btagSFlight_correlatedDown", "PUUp", "PUDown", "btagSFbc_uncorrelated_2016APVUp", "btagSFbc_uncorrelated_2016APVDown"]: continue # TMP !!!
+            if syst_name not in ["nominal", "PUUp", "PUDown", "btagSFbc_uncorrelated_2016APVUp", "btagSFbc_uncorrelated_2016APVDown"]: continue # TMP !!!
             yld_dict[cat_name][syst_name ] = {}
             for proc_name in sample_dict.keys():
                 if blind and (("data" in proc_name) and (not cat_name.startswith("cr_"))):
@@ -75,6 +78,14 @@ def handle_per_year_systs_for_fr2(in_dict, systs_special=SYSTS_SPECIAL):
             in_dict["FR2"][cat][f"{sys}Up"] = yld_up
             in_dict["FR2"][cat][f"{sys}Down"] = yld_do
 
+
+# Takes a full yield dict and returns the yields for just the procs we care about for datacard
+# Specify the year of interest, and category list, (and nominal syst is returned by default)
+def get_rate_dict(in_dict,yr_key,cat_lst):
+    out_dict = {}
+    for cat in cat_lst:
+        out_dict[cat] = in_dict[yr_key][cat]["nominal"]
+    return out_dict
 
 
 def main():
@@ -117,9 +128,112 @@ def main():
         yld_dict[year] = get_yields(histo,sample_names_dict_mc[year])
     handle_per_year_systs_for_fr2(yld_dict)
 
+    # We're only looking at Full R2 for now
+    yld_dict = yld_dict["FR2"]
 
 
-    #exit()
+    # Get just the yields we care about for now
+
+    def do_bkg_tf(in_dict):
+        print(in_dict.keys())
+
+    def get_kappa_dict(in_dict):
+
+        # Get the list of systematic base names (i.e. without the up and down tags)
+        #     - Assumes each syst has a "systnameUp" and a "systnameDown"
+        #     - This will drop nominal (since there is no "nominalUp" to tag on)
+        def get_syst_base_name_lst(in_lst):
+            out_lst = []
+            for syst in in_lst:
+                if syst.endswith("Up"):
+                    syst_name_base = syst.replace("Up","")
+                    if syst_name_base not in out_lst:
+                        out_lst.append(syst_name_base)
+            return out_lst
+
+
+        # Takes two pairs [val1,var1] and [val2,var2], returns the product with error propagated
+        # Note the var is like sumw2 i.e. alreayd squared (in both input and output)
+        def valvar_op(valvar_1, valvar_2, op):
+            #valvar_out = [None,None]
+
+            val1 = valvar_1[0]
+            var1 = valvar_1[1]
+            val2 = valvar_2[0]
+            var2 = valvar_2[1]
+
+            if op == "prod":
+                val = val1*val2
+            elif op == "div":
+                val = val1/val2
+            else:
+                raise Exception("Unknown operatation")
+
+            var = (val**2) * ( (np.sqrt(var1)/val1)**2 + (np.sqrt(var2)/val2)**2 )
+
+            return [val,var]
+
+
+        for cat in in_dict.keys():
+            print("\n",cat)
+            for sys in get_syst_base_name_lst(list(in_dict[cat].keys())):
+                print("\t",sys)
+                for proc in in_dict[cat]["nominal"]:
+                    valvar_up = in_dict[cat][f"{sys}Up"][proc]
+                    valvar_do = in_dict[cat][f"{sys}Down"][proc]
+                    valvar_nom = in_dict[cat]["nominal"][proc]
+                    valvar_kappa_up = valvar_op(valvar_up,valvar_nom,"div")
+                    valvar_kappa_do = valvar_op(valvar_do,valvar_nom,"div")
+                    print("\t\t",proc,valvar_nom)
+                    print("\t\t",proc,valvar_up)
+                    print("\t\t",proc,valvar_do)
+                    print("\t\t",proc,valvar_kappa_up)
+                    print("\t\t",proc,valvar_kappa_do)
+
+
+    tf_info_dict = {
+
+        'sr_4l_sf_A' : {
+            "ZZ" : {
+                "sr" : "sr_sf_all",
+                "cr" : "cr_4l_sf",
+            },
+            "ttZ" : {
+                "sr" : "sr_sf_all",
+                "cr" : "cr_4l_btag_sf_offZ_met80",
+            },
+        },
+    }
+
+    #for cat in tf_info_dict:
+        #zz_yld = n_cr * mc_sr / mc_cr
+
+    #'sr_4l_sf_B' :
+    #'sr_4l_sf_C' :
+
+    #'sr_4l_of_1' :
+    #'sr_4l_of_2' :
+    #'sr_4l_of_3' :
+    #'sr_4l_of_4' :
+
+    #'cr_4l_btag_of'
+    #'cr_4l_btag_sf_offZ_met80'
+    #'cr_4l_sf'
+
+
+    #x = do_bkg_tf(yld_dict)
+    x = get_kappa_dict(yld_dict)
+
+
+
+
+    #rate_dict = get_rate_dict(yld_dict,yr_key="FR2",cat_lst=CAT_LST_CB)
+    #kappa_dict = get_kappa_dict(yld_didct
+    #print("Before",yld_dict)
+    #print("After",rate_dict)
+
+
+    exit()
     ### Print stuff
     print("")
     print(yld_dict)
