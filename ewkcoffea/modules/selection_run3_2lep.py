00bd4a40c7ef50f8327993e26b42013c9d4cc0df
MatthewDittrich <matthew.dittrich@ufl.edu>
2024-03-27 16:41:42 -0400

    adding correct selection_run3_2lep.py

--- ewkcoffea/modules/selection_run3_2lep.py
+++ ewkcoffea/modules/selection_run3_2lep.py
@@ -1,9 +1,4 @@
-import numpy as np
 import awkward as ak
-
-
-
-import topcoffea.modules.event_selection as tc_es
 
 from ewkcoffea.modules.paths import ewkcoffea_path
 from topcoffea.modules.get_param_from_jsons import GetParam
@@ -16,11 +11,34 @@ dataset_dict = {
     "2022" : {
         "EGamma" : [
             "Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ",
+            "Ele23_Ele12_CaloIdL_TrackIdL_IsoVL",
+            "Ele30_WPTight_Gsf",
+            "Ele32_WPTight_Gsf",
+            "Ele32_WPTight_Gsf_L1DoubleEG",
+            "Ele35_WPTight_Gsf",
+            "Ele115_CaloIdVT_GsfTrkIdT",
+            "DoubleEle25_CaloIdL_MW",
+            "DoublePhoton70",
+        ],
+        "SingleMuon" : [
+            "IsoMu24",
+            "IsoMu27",
+            "Mu50",
+        ],
+        "DoubleMuon" : [
+            "Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8",
+            "Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8",
         ],
         "Muon" : [
             "Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8",
+            "Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8",
+            "IsoMu24",
+            "IsoMu27",
+            "Mu50",
         ],
         "MuonEG" : [
+            "Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ",
+            "Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL",
             "Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ",
             "Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ",
         ]
@@ -28,138 +46,80 @@ dataset_dict = {
 
 }
 
-trgs_for_matching = {
-
-    "2022" : {
-        "m_m" : {
-            "trg_lst" : ["Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8"],
-            "offline_thresholds" : [10.0,10.0],
-        },
-        "e_e" : {
-            "trg_lst" : ["Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ"],
-            "offline_thresholds" : [10.0,10.0],
-        },
-        "m_e" : {
-            "trg_lst" : ["Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ"],
-            "offline_thresholds" : [10.0,10.0],
-        },
-        "e_m" : {
-            "trg_lst" : ["Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ"],
-            "offline_thresholds" : [10.0,10.0],
-        },
-    }
-}
-
 
 # Hard coded dictionary for figuring out overlap...
 #   - No unique way to do this
 #   - Note: In order for this to work properly, you should be processing all of the datastes to be used in the analysis
 #   - Otherwise, you may be removing events that show up in other datasets you're not using
+
+# Note: The reason for era dependency in 2022 is the change from having a SingleMuon and DoubleMuon dataset to just a Muon dataset in era C
+# For Era C which has both, the events in (SingleMuon, DoubleMuon) and (Muon) are exclusive so we do not perform duplicate removal between these sets
+# For Era C, SingleMuon and DoubleMuon fall in the run ranges of [355800,357399] while Muon falls in [356400,357400]
 exclude_dict = {
-    "2022": {
+    "B": {
+        "SingleMuon"     : [],
+        "DoubleMuon"     : dataset_dict["2022"]["SingleMuon"],
+        "EGamma"         : dataset_dict["2022"]["SingleMuon"] + dataset_dict["2022"]["DoubleMuon"],
+        "MuonEG"         : dataset_dict["2022"]["SingleMuon"] + dataset_dict["2022"]["DoubleMuon"] + dataset_dict["2022"]["EGamma"],
+    },
+    "C": {
+        "Muon"           : [],
+        "SingleMuon"     : [],
+        "DoubleMuon"     : dataset_dict["2022"]["SingleMuon"],
+        "EGamma"         : dataset_dict["2022"]["Muon"] + dataset_dict["2022"]["DoubleMuon"] + dataset_dict["2022"]["SingleMuon"],
+        "MuonEG"         : dataset_dict["2022"]["Muon"] + dataset_dict["2022"]["DoubleMuon"] + dataset_dict["2022"]["SingleMuon"] + dataset_dict["2022"]["EGamma"],
+    },
+    "D": {
+        "Muon"     : [],
+        "EGamma"         : dataset_dict["2022"]["Muon"],
+        "MuonEG"         : dataset_dict["2022"]["Muon"] + dataset_dict["2022"]["EGamma"],
+    },
+    "E": {
+        "Muon"     : [],
+        "EGamma"         : dataset_dict["2022"]["Muon"],
+        "MuonEG"         : dataset_dict["2022"]["Muon"] + dataset_dict["2022"]["EGamma"],
+    },
+    "F": {
+        "Muon"     : [],
+        "EGamma"         : dataset_dict["2022"]["Muon"],
+        "MuonEG"         : dataset_dict["2022"]["Muon"] + dataset_dict["2022"]["EGamma"],
+    },
+    "G": {
         "Muon"     : [],
         "EGamma"         : dataset_dict["2022"]["Muon"],
         "MuonEG"         : dataset_dict["2022"]["Muon"] + dataset_dict["2022"]["EGamma"],
     },
 }
 
-#------------------------------------------------------------------------------------------------------------------------------
-# Apply trigger matching requirements to make sure pt is above online thresholds
-def trg_matching(events,year):
-    if year == "2022EE": year = "2022"
-    # Initialize return array to be True array with same shape as events
-    ret_arr = ak.zeros_like(np.array(events.event), dtype=bool)
-
-    # Get the leptons, sort and pad
-    el = events.l_Run3_2Lep_t[abs(events.l_Run3_2Lep_t.pdgId)==11]
-    el = ak.pad_none(el[ak.argsort(el.pt,axis=-1,ascending=False)],2)
-    mu = events.l_Run3_2Lep_t[abs(events.l_Run3_2Lep_t.pdgId)==13]
-    mu = ak.pad_none(mu[ak.argsort(mu.pt,axis=-1,ascending=False)],2)
-
-    # Loop over offline cuts, make sure triggers pass the offline cuts for the associated triggers
-    for l_l in trgs_for_matching[year]:
-
-
-        # Check if lep pt passes the offline cuts
-        offline_thresholds = trgs_for_matching[year][l_l]["offline_thresholds"]
-        if   l_l == "m_m": offline_cut = ak.fill_none(((mu[:,0].pt > offline_thresholds[0]) & (mu[:,1].pt > offline_thresholds[1])),False)
-        elif l_l == "e_e": offline_cut = ak.fill_none(((el[:,0].pt > offline_thresholds[0]) & (el[:,1].pt > offline_thresholds[1])),False)
-        elif l_l == "m_e": offline_cut = ak.fill_none(((mu[:,0].pt > offline_thresholds[0]) & (el[:,0].pt > offline_thresholds[1]) & (mu[:,0].pt > el[:,0].pt )),False)
-        elif l_l == "e_m": offline_cut = ak.fill_none(((el[:,0].pt > offline_thresholds[0]) & (mu[:,0].pt > offline_thresholds[1]) & (mu[:,0].pt < el[:,0].pt )),False)
-        else: raise Exception("Unknown offline cut.")
-
-        # Check if trigger passes the associated triggers
-        trg_lst = trgs_for_matching[year][l_l]["trg_lst"]
-        trg_passes = tc_es.passes_trg_inlst(events,trg_lst)
-
-        # Build the return mask
-        # The return mask started from an array of False
-        # The way an event becomes True is if it passes a trigger AND passes the offline pt cuts associated with that trg
-        false_arr = ak.zeros_like(np.array(events.event), dtype=bool) # False array with same shape as events
-        ret_arr = ret_arr | ak.where(trg_passes,offline_cut,false_arr)
-
-    return ret_arr
-
-#------------------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------------
 # 2Lep Selection
-def add2lmask_Run3_2Lep(events, year, isData):
+def add2lmask_run3_2lep(events, year, isData):
 
     # Leptons and padded leptons
-    leps = events.l_Run3_2Lep_t
+    leps = events.l_run3_2lep_veto
     leps_padded = ak.pad_none(leps,2)
 
     # Filters
     filter_flags = events.Flag
-    filters = filter_flags.goodVertices & filter_flags.globalSuperTightHalo2016Filter & filter_flags.HBHENoiseFilter & filter_flags.HBHENoiseIsoFilter & filter_flags.EcalDeadCellTriggerPrimitiveFilter & filter_flags.BadPFMuonFilter & filter_flags.ecalBadCalibFilter
+    # Filters are from AN SMP-24-001
+    filters = filter_flags.goodVertices & filter_flags.globalSuperTightHalo2016Filter & filter_flags.EcalDeadCellTriggerPrimitiveFilter & filter_flags.BadPFMuonFilter & filter_flags.ecalBadCalibFilter & filter_flags.BadPFMuonDzFilter & filter_flags.hfNoisyHitsFilter & filter_flags.eeBadScFilter
 
     # Lep multiplicity
     nlep_2 = (ak.num(leps) == 2)
 
     mask = filters & nlep_2
 
-    events['muon_sf'] = leps_padded[:,0].muon_sf*leps_padded[:,1].muon_sf
-    events['ele_sf'] = leps_padded[:,0].ele_sf*leps_padded[:,1].ele_sf
+    events['muon_sf'] = leps_padded[:,0].sf_nom_muon*leps_padded[:,1].sf_nom_muon
+    events['ele_sf'] = leps_padded[:,0].sf_nom_elec*leps_padded[:,1].sf_nom_elec
     events['is2l'] = ak.fill_none(mask,False)
 
-
-#------------------------------------------------------------------------------------------------------------------------------
-# nJet Selection
-def addjetmask_Run3_2Lep(events, year, isData):
-
-    jets = events.jets_Run3_2Lep
-
-    njet_2 = (ak.num(jets) >= 2)
-
-    mask = njet_2
-    events['has2jets'] = ak.fill_none(mask,False)
-#------------------------------------------------------------------------------------------------------------------------------
-# Leading and Subleading Jet Selection
-def addjetispresent_Run3_2Lep(jet0, jet1, jets_Run3_2Lep):
-
-    njet_1 = (ak.num(jets_Run3_2Lep) >= 1)
-    njet_2 = (ak.num(jets_Run3_2Lep) >= 2)
-
-    mask1 = njet_1
-    mask2 = njet_2
-    jet0['has1jet'] = ak.fill_none(mask1,False)
-    jet1['has2jet'] = ak.fill_none(mask2,False)
-#------------------------------------------------------------------------------------------------------------------------------
-# Met Mask
-def addmetmask_Run3_2Lep(events, year, isData):
-
-    met = events.MET
-
-    met_30 = (met.pt <= 30)
-
-    mask = met_30
-    events['metmask'] = ak.fill_none(mask,False)
 #------------------------------------------------------------------------------------------------------------------------------
 # Do Run3 2Lep pre selection, construct event level mask
-# Convenience function around get_Run3_2Lep_candidates() and get_z_candidate_mask()
-def attach_Run3_2Lep_preselection_mask(events,lep_collection):
+def attach_run3_2lep_preselection_mask(events,lep_collection):
 
     # Pt requirements (assumes lep_collection is pt sorted and padded)
-    pt_mask = ak.fill_none((lep_collection[:,0].pt > 25.0) & (lep_collection[:,1].pt > 15.0),False)
+    pt_mask = ak.fill_none((lep_collection[:,0].pt > 25.0),False)
+    pt_mask = ak.fill_none(pt_mask,False) # Replace the None with False in the mask just to make it easier to think about
 
     # SFOS and OPOS masks
     os_mask = ak.any((((lep_collection[:,0:1].pdgId)*(lep_collection[:,1:2].pdgId)) < 0),axis=1) # Use ak.any() here so that instead of e.g [[None],None,...] we have [False,None,...]
@@ -175,18 +135,18 @@ def attach_Run3_2Lep_preselection_mask(events,lep_collection):
     mumu_mask = ak.fill_none(mumu_mask,False) # Replace the None with False in the mask just to make it easier to think about
 
     # mLL masks
-    z_mass_mask = ak.any((abs(((lep_collection[:,0:1] + lep_collection[:,1:2]).mass) - 91.2) < 20.0),axis=1) # Use ak.any() here so that instead of e.g [[None],None,...] we have [False,None,...]
+    z_mass_mask = ak.any((abs(((lep_collection[:,0:1] + lep_collection[:,1:2]).mass) - get_ec_param("zmass")) < 15.0),axis=1) # Use ak.any() here so that instead of e.g [[None],None,...] we have [False,None,...]
     z_mass_mask = ak.fill_none(z_mass_mask,False) # Replace the None with False in the mask just to make it easier to think about
 
     of_mass_mask = ak.any((((lep_collection[:,0:1] + lep_collection[:,1:2]).mass) > 20.0),axis=1) # Use ak.any() here so that instead of e.g [[None],None,...] we have [False,None,...]
     of_mass_mask = ak.fill_none(of_mass_mask,False) # Replace the None with False in the mask just to make it easier to think about
 
     # The final preselection mask
-    Run3_2Lep_presel_mask = (os_mask & pt_mask)
+    run3_2lep_presel_mask = (os_mask & pt_mask)
 
     # Attach to the lepton objects
-    events["Run3_2Lep_presel_sf_ee"] = (Run3_2Lep_presel_mask & sf_mask & ee_mask & z_mass_mask)
-    events["Run3_2Lep_presel_sf_mumu"] = (Run3_2Lep_presel_mask & sf_mask & mumu_mask & z_mass_mask)
-    events["Run3_2Lep_presel_of"] = (Run3_2Lep_presel_mask & of_mask & of_mass_mask)
+    events["run3_2lep_presel_sf_ee"] = (run3_2lep_presel_mask & sf_mask & ee_mask & z_mass_mask)
+    events["run3_2lep_presel_sf_mumu"] = (run3_2lep_presel_mask & sf_mask & mumu_mask & z_mass_mask)
+    events["run3_2lep_presel_of"] = (run3_2lep_presel_mask & of_mask & of_mass_mask)
 
 
