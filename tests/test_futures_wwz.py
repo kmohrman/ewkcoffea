66ad2328be17abbe74b17602bd10b27c0608dbd9
Kelci <k.mohrman@ufl.edu>
2024-04-04 14:33:03 -0400

    First pass at CI for R3

--- tests/test_futures_wwz.py
+++ tests/test_futures_wwz.py
@@ -1,7 +1,7 @@
 import subprocess
 from os.path import exists
 
-def test_topcoffea():
+def test_ewkcoffea_r2():
     args = [
         "time",
         "python",
@@ -15,7 +15,26 @@ def test_topcoffea():
         "analysis/wwz/histos/"
     ]
 
-    # Run TopCoffea
+    # Run ewkcoffea
     subprocess.run(args)
 
     assert (exists('analysis/wwz/histos/output_check_yields.pkl.gz'))
+
+def test_ewkcoffea_2022():
+    args = [
+        "time",
+        "python",
+        "analysis/wwz/run_wwz4l.py",
+        "-x",
+        "futures",
+        "input_samples/sample_jsons/test_samples/2022_WWZJetsTo4L2Nu_forCI.json",
+        "-o",
+        "output_check_yields_2022",
+        "-p",
+        "analysis/wwz/histos/"
+    ]
+
+    # Run ewkcoffea
+    subprocess.run(args)
+
+    assert (exists('analysis/wwz/histos/output_check_yields_2022.pkl.gz'))
