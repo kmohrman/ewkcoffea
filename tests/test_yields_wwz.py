ec25c40e80160da8f10b48af1a8cce62a5007e5f
Kelci <k.mohrman@ufl.edu>
2024-04-04 14:49:20 -0400

    Add the ref yields check for 2022

--- tests/test_yields_wwz.py
+++ tests/test_yields_wwz.py
@@ -1,7 +1,9 @@
 import subprocess
 from os.path import exists
 
-def test_make_yields_after_processor_wwz():
+### Test for a R2 sample ###
+
+def test_make_yields_after_processor_wwz_r2():
     assert (exists('analysis/wwz/histos/output_check_yields.pkl.gz')) # Make sure the input pkl file exists
 
     args = [
@@ -17,12 +19,49 @@ def test_make_yields_after_processor_wwz():
     subprocess.run(args)
     assert (exists('analysis/wwz/output_check_yields.json'))
 
-def test_compare_yields_after_processor_wwz():
+def test_compare_yields_after_processor_wwz_r2():
     args = [
         "python",
         "analysis/wwz/comp_json_yields.py",
         "analysis/wwz/output_check_yields.json",
         "analysis/wwz/ref_for_ci/counts_wwz_ref.json",
+        "-t1",
+        "New yields",
+        "-t2",
+        "Ref yields"
+    ]
+
+    # Run comparison
+    out = subprocess.run(args, stdout=True)
+    assert (out.returncode == 0) # Returns 0 if all pass
+
+
+### Test for a R3 sample ###
+
+def test_make_yields_after_processor_wwz_2022():
+    assert (exists('analysis/wwz/histos/output_check_yields_2022.pkl.gz')) # Make sure the input pkl file exists
+
+    args = [
+        "python",
+        "analysis/wwz/get_wwz_counts.py",
+        "-f",
+        "analysis/wwz/histos/output_check_yields_2022.pkl.gz",
+        "-n",
+        "analysis/wwz/output_check_yields_2022",
+        "-s",
+        "2022_WWZJetsTo4L2Nu",
+    ]
+
+    # Produce json
+    subprocess.run(args)
+    assert (exists('analysis/wwz/output_check_yields.json'))
+
+def test_compare_yields_after_processor_wwz_2022():
+    args = [
+        "python",
+        "analysis/wwz/comp_json_yields.py",
+        "analysis/wwz/output_check_yields_2022.json",
+        "analysis/wwz/ref_for_ci/counts_wwz_ref_2022.json",
         "-t1",
         "New yields",
         "-t2",
