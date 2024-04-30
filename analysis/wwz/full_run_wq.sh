5d654e3592663a9efb21ae506d0f42a8ecb8c7b1
Kelci <k.mohrman@ufl.edu>
2024-04-29 21:39:19 -0400

    Update full run script

--- analysis/wwz/full_run_wq.sh
+++ analysis/wwz/full_run_wq.sh
@@ -1,2 +1,7 @@
+# Some example commands for running at scale
+
 # Run at scale (with wq)
-time python run_wwz4l.py ../../input_samples/cfgs/wwz_analysis/mc_sig_bkg_samples.cfg,../../input_samples/cfgs/wwz_analysis/data_samples.cfg -o wwz_histos
+time python run_wwz4l.py ../../input_samples/cfgs/wwz_analysis/mc_sig_bkg_samples.cfg,../../input_samples/cfgs/wwz_analysis/data_samples.cfg -o wwz_histos --do-systs --hist-list few
+
+# Run at scale (with wq) with hpg copy
+#time python run_wwz4l.py ../../input_samples/cfgs/wwz_analysis/mc_sig_bkg_samples_hpg.cfg,../../input_samples/cfgs/wwz_analysis/data_samples_hpg.cfg -o wwz_histos_hpg --do-systs --hist-list few
