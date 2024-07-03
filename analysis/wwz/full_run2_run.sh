fbd1578119cd756ce4bce1a2f9135b20fde14c41
Kelci Mohrman <kelci.mohrman@cern.ch>
2024-07-02 20:15:28 -0700

    We want to use the bdt list of variables not the few list

--- analysis/wwz/full_run2_run.sh
+++ analysis/wwz/full_run2_run.sh
@@ -7,7 +7,7 @@
 #time python run_wwz4l.py ../../input_samples/cfgs/wwz_analysis/mc_sig_bkg_samples.cfg,../../input_samples/cfgs/wwz_analysis/data_samples.cfg -o wwz_histos -x futures -n 128
 
 # Run at scale with wq
-#time python run_wwz4l.py ../../input_samples/cfgs/wwz_analysis/mc_sig_bkg_samples.cfg,../../input_samples/cfgs/wwz_analysis/data_samples.cfg -o wwz_histos --do-systs --hist-list few
+#time python run_wwz4l.py ../../input_samples/cfgs/wwz_analysis/mc_sig_bkg_samples.cfg,../../input_samples/cfgs/wwz_analysis/data_samples.cfg -o wwz_histos --do-systs --hist-list bdt
 
 # Run at scale with wq, with hpg copy
 #time python run_wwz4l.py ../../input_samples/cfgs/wwz_analysis/mc_sig_bkg_samples_hpg.cfg,../../input_samples/cfgs/wwz_analysis/data_samples_hpg.cfg -o wwz_histos_hpg --do-systs --hist-list njets njets_counts
@@ -16,7 +16,7 @@
 ### Examples with 4l skims configs ###
 
 # Run at scale with futures
-#time python run_wwz4l.py ../../input_samples/cfgs/wwz_analysis/samples_4lskim.cfg -x futures -n 200 -s 100000000 -o wwz_histos --hist-list few
+#time python run_wwz4l.py ../../input_samples/cfgs/wwz_analysis/samples_4lskim.cfg -x futures -n 200 -s 100000000 -o wwz_histos --hist-list bdt
 #time python run_wwz4l.py ../../input_samples/cfgs/wwz_analysis/samples_4lskim.cfg -x futures -n 200 -s 100000000 -o wwz_histos --hist-list njets njets_counts --do-systs
 
 # Run with the siphon turned on (might want to comment out data in the input cfg)
