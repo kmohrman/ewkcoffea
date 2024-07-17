3fabff71d09fe0393a7c01ef8414e29c75b7d4fd
Matthew Dittrich <matthew.dittrich@ufl.edu>
2024-07-17 05:00:51 -0700

    UAF sample jsons and cfgs

--- analysis/wwz/full_run3_run.sh
+++ analysis/wwz/full_run3_run.sh
@@ -2,7 +2,9 @@
 
 # Run at scale (with futures) DO NOT RUN THIS ON LOGIN NODE AT UF!
 #time python run_wwz4l.py ../../input_samples/cfgs/wwz_analysis/mc_sig_bkg_samples_run3.cfg,../../input_samples/cfgs/wwz_analysis/data_samples_run3.cfg -o wwz_run3_histos_nosyst -x futures -n 200
-time python run_wwz4l.py ../../input_samples/cfgs/wwz_analysis/mc_sig_bkg_samples_run3.cfg,../../input_samples/cfgs/wwz_analysis/data_samples_run3.cfg -o wwz_run3_histos_withsyst -x futures -n 200 --hist-list njets njets_counts --do-systs
+
+# Run at scale (with futures) locally on UAF -> WILL NOT WORK ON HPG!
+time python run_wwz4l.py ../../input_samples/cfgs/wwz_analysis/mc_sig_bkg_samples_run3_uaf.cfg,../../input_samples/cfgs/wwz_analysis/data_samples_run3_uaf.cfg -o wwz_run3_histos_nosyst -x futures -n 200
 
 # Run with the siphon on DO NOT RUN THIS ON LOGIN NODE AT UF!
 #time python run_wwz4l.py ../../input_samples/cfgs/wwz_analysis/mc_sig_bkg_samples_run3.cfg -o wwz_run3_histos_siphon -x futures -n 200 --siphon
