97015cdfd0cfcf5db7823f8ec1bd51ff3084059e
Matthew Dittrich <matthew.dittrich@ufl.edu>
2024-07-29 05:21:52 -0700

    Merging the run scripts

--- analysis/wwz/full_run3_run.sh
+++ analysis/wwz/full_run3_run.sh
@@ -9,3 +9,9 @@ time python run_wwz4l.py ../../input_samples/cfgs/wwz_analysis/samples_run3.cfg 
 
 # Run at scale (with wq)
 #time python run_wwz4l.py ../../input_samples/cfgs/wwz_analysis/samples_run3.cfg -o wwz_run3_histos --hist-list few
+
+#Run at scale (with futures) for JUST 2022
+#time python run_wwz4l.py ../../input_samples/cfgs/wwz_analysis/samples_2022.cfg -o wwz_2022_histos -x futures -n 200
+
+# Run at scale (with futures) for JUST 2023
+#time python run_wwz4l.py ../../input_samples/cfgs/wwz_analysis/samples_2023.cfg -o wwz_2023_histos -x futures -n 200
