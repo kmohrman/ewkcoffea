acbb4aa1cf5c0db411033d50b027cd9e32cf4172
Kelci Mohrman <kelci.mohrman@cern.ch>
2024-09-16 11:51:59 -0700

    Handle the de correlation of fake WZ uncty

--- analysis/wwz/make_datacards.py
+++ analysis/wwz/make_datacards.py
@@ -248,16 +248,26 @@ def get_rate_systs(proc_lst,year_tag):
 
     # Make rows for rate uncertainties that impact a subset of the processes
     for uncty_name in rate_systs_dict["rate_uncertainties_some_proc"]:
+
+        # If the uncty has a decorrelation_dict defined, use that to append a tag to the name of the uncty
+        # E.g., for the fake uncertianty, we append the run so that it is fake_run2 or fake_run3
+        if "decorrelation_dict" in rate_systs_dict["rate_uncertainties_some_proc"][uncty_name]:
+            tag = "_" + rate_systs_dict["rate_uncertainties_some_proc"][uncty_name]["decorrelation_dict"][year_tag]
+        else:
+            uncty_name_tag = ""
+
+        # Loop over procs and put in the uncty or "-" if NA
         for proc_of_interest in rate_systs_dict["rate_uncertainties_some_proc"][uncty_name]["procs"]:
-            out_dict[f"{uncty_name}_{proc_of_interest}"] = {}
+            out_dict[f"{uncty_name}_{proc_of_interest}{tag}"] = {}
             for proc_itr in proc_lst:
                 if proc_itr == proc_of_interest:
                     uncty = str(rate_systs_dict["rate_uncertainties_some_proc"][uncty_name]["val"])
                 else:
                     uncty = "-"
-                out_dict[f"{uncty_name}_{proc_of_interest}"][proc_itr] = uncty
+                out_dict[f"{uncty_name}_{proc_of_interest}{tag}"][proc_itr] = uncty
 
     return out_dict
+
 
 #Determines if the up and down variations of a systematic are in the same direction
 def determine_updo_same(nom,up,down):
