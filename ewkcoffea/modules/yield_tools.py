11a4b1221e0bbc536c132670bbbe018c74c99c10
Matthew Dittrich <matthew.dittrich@ufl.edu>
2024-12-17 07:18:45 -0800

    Making zeroing low mc bins an optional argument

--- ewkcoffea/modules/yield_tools.py
+++ ewkcoffea/modules/yield_tools.py
@@ -34,7 +34,7 @@ def valvar_op(valvar_1, valvar_2, op):
 ############## Getting yields from a histo ##############
 
 # Get the yields (nested in the order: year,cat,syst,proc)
-def get_yields(histo,sample_dict,blind=True,systematic_name=None):
+def get_yields(histo,sample_dict,blind=True,zero_low_mc = False,systematic_name=None):
 
     yld_dict = {}
 
@@ -58,7 +58,7 @@ def get_yields(histo,sample_dict,blind=True,systematic_name=None):
                     var = sum(sum(histo[{"category":cat_name,"process":sample_dict[proc_name],"systematic":syst_name }].variances(flow=True)))
 
                     # Zero out bins that are consistent with zero in the nominal
-                    if (np.sqrt(var_n) >= val_n):
+                    if (np.sqrt(var_n) >= val_n) and zero_low_mc and ("data" not in proc_name):
                         val = 0
                         var = 0
 
