54f07a4e0a3fe87695939f4f1b2d8b281ee2ff56
Kelci Mohrman <kelci.mohrman@cern.ch>
2024-12-18 15:56:08 -0800

    Update dc maker wrapper

--- analysis/wwz/make_datacards_wrapper.sh
+++ analysis/wwz/make_datacards_wrapper.sh
@@ -5,9 +5,13 @@ R3_PKL="r3_wwz_histos_withSyst.pkl.gz"
 
 rm cards_wwz4l/*
 
-# For BDT results
-python make_datacards.py histos/$R2_PKL -u run2 -s --bdt
-python make_datacards.py histos/$R3_PKL -u run3 -s --bdt
+# For unblind BDT results
+python make_datacards.py histos/$R2_PKL -u run2 -s --bdt --unblind --zero-low-mc
+python make_datacards.py histos/$R3_PKL -u run3 -s --bdt --unblind --zero-low-mc
+
+# For blind BDT results
+#python make_datacards.py histos/$R2_PKL -u run2 -s --bdt --zero-low-mc
+#python make_datacards.py histos/$R3_PKL -u run3 -s --bdt --zero-low-mc
 
 # For Cut Based results
 #python make_datacards.py histos/$R2_PKL -u run2 -s
